8de6677e1005ea5cebb9a015e67890c04a5cad3a
aycollapse <81759055+aycollapse@users.noreply.github.com>
2025-06-11 12:56:38 +0200

    feat: abstract media class

--- src/core/models/abstract_media.cpp
+++ src/core/models/abstract_media.cpp
@@ -1,9 +1,20 @@
 #include "abstract_media.h"
 
-AbstractMedia::AbstractMedia() : id(QUuid::createUuid()) {}
+AbstractMedia::AbstractMedia() : id(QUuid::createUuid()), date_added(QDate::currentDate()) {}
 AbstractMedia::~AbstractMedia() {}
 
 // getters
 const QUuid& AbstractMedia::getId() const {return id;}
 const QString& AbstractMedia::getName() const {return name;}
-const QDate& AbstractMedia::getDate() const {return date;}
+const float& AbstractMedia::getRating() const {return rating;}
+const QString AbstractMedia::getDescription() const {return description;}
+const QString AbstractMedia::getComment() const {return comment;}
+const QDate& AbstractMedia::getDateRelease() const {return date_release;}
+const QDate& AbstractMedia::getDateAdded() const {return date_added;}
+
+//setters
+void AbstractMedia::setName(const QString& name){this->name = name;}
+void AbstractMedia::setRating(const float& rating){this->rating = rating;}
+void AbstractMedia::setDescription(const QString& description){this->description = description;}
+void AbstractMedia::setComment(const QString& comment){this->comment = comment;}
+void AbstractMedia::setDateRelease(const QDate& date){this->date_release = date;}
--- src/core/models/abstract_media.h
+++ src/core/models/abstract_media.h
@@ -13,16 +13,31 @@ public:
     AbstractMedia();
     virtual ~AbstractMedia() = 0;
     virtual void accept(Visitor& visitor) = 0;
-    
+
     //getters
-    const QUuid& getId() const;
+    const QUuid& getId() const; //set with constructor
     const QString& getName() const;
-    const QDate& getDate() const;
+    const float& getRating() const;
+    const QString getDescription() const;
+    const QString getComment() const;
+    const QDate& getDateRelease() const;
+    const QDate& getDateAdded() const; //set with constructor
+
+    //setters
+    void setName(const QString& name);
+    void setRating(const float& rating);
+    void setDescription(const QString& description);
+    void setComment(const QString& comment);
+    void setDateRelease(const QDate& date);
 
 protected:
     QUuid id;
     QString name;
-    QDate date;
+    float rating;
+    QString description;
+    QString comment;
+    QDate date_release;
+    QDate date_added;
 };
 
 #endif // ABSTRACT_MEDIA_H
